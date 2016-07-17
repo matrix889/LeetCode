25c99683ca34f15f4af621c7c0b6776c171b1d47
matrix889 <397470071@qq.com>
2016-07-17 22:00:56 +0800

    LeetCode 371: 题目大意：     不使用+，-运算符，实现32整数的加法运算。 思路：     不考虑进位，a+b的结果为 a xor b；而 a + b的进位为 (a and b) << 1。还需要注意到 （a xor b）+ (a and b) << 1 也可能产生进位，即需要递归求解直到进位为0。

---
+++ CSharp/CSharp/P371.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P371
+{
+    class Solution
+    {
+        public int GetSum(int a, int b)
+        {
+            int result = a ^ b;
+            int carry = (a & b) << 1;
+
+            return (carry == 0) ? result : GetSum(result, carry);
+        }
+    }
+
+}
--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -10,6 +10,13 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            P371.Solution test = new P371.Solution();
+            Console.WriteLine(test.GetSum(a, b));
+            Console.WriteLine(a + b);
+
         }
     }
 }
