9e67f06a082ccf7a77f2a9804ddafceb6161ac58
matrix889 <397470071@qq.com>
2016-07-17 22:00:56 +0800

    LeetCode 374: 题目大意：     给出一个字符串，将它的全部元音字母的反转，如给出 s = "leetcode", 返回 "leotcede". 思路：     设置两个变量l,r分别从头尾开始遍历，找到元音字母便交换。一开始没有考虑大小写的问题。。。

---
+++ CSharp/CSharp/P345.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P345
+{
+
+    public class Solution
+    {
+        private char []_vowel;
+        public string ReverseVowels(string s)
+        {
+            int l = 0, r = s.Length - 1;
+            _vowel = new char[5] { 'a', 'e', 'i', 'o', 'u' };
+            var tmpS = new char[s.Length];
+            for (var i = 0; i < s.Length; ++i) tmpS[i] = s[i];
+
+            while (l < r)
+            {
+                while (l < s.Length && !IsVowel(s[l])) ++l;
+                while (r > 0 && !IsVowel(s[r])) --r;
+                if (l < r)
+                {
+                    var tmp = tmpS[l];
+                    tmpS[l] = tmpS[r];
+                    tmpS[r] = tmp;
+                }
+                ++l;
+                --r;
+            }
+            return new string(tmpS);
+        }
+
+        private bool IsVowel(char c)
+        {
+            for (var index = 0; index < _vowel.Length; index++)
+            {
+                var t = _vowel[index];
+                if ((c == t) || (t == char.ToLower(c)) ) return true;
+            }
+            return false;
+        }
+    }
+}
--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -10,6 +10,13 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
+            P345.Solution tmp = new P345.Solution();
+            for (var index = 0; index < 5; ++index)
+            {
+                string s = Console.ReadLine();
+                Console.WriteLine(tmp.ReverseVowels(s));
+            }
+
         }
     }
 }
